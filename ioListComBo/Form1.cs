ba6652feaf7ce7f143bd04bdc734270caabc2e26
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-08-18 15:00:58 +0900

    데이터 바인딩

--- ioListComBo/Form1.cs
+++ ioListComBo/Form1.cs
@@ -15,11 +15,18 @@ namespace ioListComBo
         public Form1()
         {
             InitializeComponent();
+            productBindingSource.Add(new Product() { Name = "감자", Price = 500 });
+            productBindingSource.Add(new Product() { Name = "사과", Price = 200 });
+            productBindingSource.Add(new Product() { Name = "고구마", Price = 700 });
+            productBindingSource.Add(new Product() { Name = "배추", Price = 600 });
+            productBindingSource.Add(new Product() { Name = "감자", Price = 500 });
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+
     }
 }
